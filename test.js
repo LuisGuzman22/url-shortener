e6f03838d6ab62dc3963b480ae28952a886eeccb
Luis Guzmán <luisguzman@MacBook-Pro-de-Luis.local>
2024-07-25 09:55:55 -0400

    add replicas

--- test.js
+++ test.js
@@ -3,8 +3,8 @@ import { check, sleep } from "k6";
 
 export let options = {
   stages: [
-    { duration: "60s", target: 100 }, // Ramp-up to 5000 users in 5 seconds
-    { duration: "10s", target: 100 }, // Stay at 5000 users for 10 seconds
+    { duration: "10s", target: 2000 }, // Ramp-up to 1000 users in 5 seconds
+    { duration: "60s", target: 5000 }, // Stay at 1000 users for 10 seconds
     { duration: "5s", target: 0 }, // Ramp-down to 0 users in 5 seconds
   ],
 };
